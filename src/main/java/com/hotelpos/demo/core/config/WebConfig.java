94cb40e7b38d4a0f2c69be8bff87d0d2ff3fc0c8
Adam <lijalem525@gmail.com>
2026-07-19 20:27:52 +0300

    commit

--- src/main/java/com/hotelpos/demo/core/config/WebConfig.java
+++ src/main/java/com/hotelpos/demo/core/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Applies to every endpoint in your app
-                .allowedOrigins("http://localhost:4200", "https://onrender.com")
+                .allowedOrigins("http://localhost:4200", "https://onrender.com" ,"https://hotel-app-front-2xh0.onrender.com" )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
