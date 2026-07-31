65374061379df27441f9356d20c4800b55cdf193
Adam <lijalem525@gmail.com>
2026-07-31 23:05:02 +0300

    commit

--- src/main/java/com/hotelpos/demo/shared/BaseEntity.java
+++ src/main/java/com/hotelpos/demo/shared/BaseEntity.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.ParamDef;
 
+
 @Data
 @MappedSuperclass
 @FilterDef(name = "tenantFilter", parameters = @ParamDef(name = "tenantId", type = String.class))
